ff4c465c829cefc6ee0d0d602f109048ab2bb625
Lucas Merchant <lbmerchant93@gmail.com>
2021-04-10 17:45:33 -0600

    Build out basic BarChart Component

---
+++ components/BarChart.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+//import PropTypes from 'prop-types';
+
+class BarChart extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <>
+            <h2>Bar Chart</h2>
+            </>
+        )
+    }
+
+}
+
+export default BarChart;
+
+//PropTypes
+//BarChart.propTypes = {
+
+//}
