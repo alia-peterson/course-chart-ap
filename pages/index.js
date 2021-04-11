72cd5f051cac9aff081f7db1c636c2050ccc7861
Cameron Aragon <caragon4695@gmail.com>
2021-04-10 22:17:00 -0600

    Import the circle chart component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import styles from '../styles/Home.module.scss'
 
-
 export default function Home() {
   return (
       <main className={styles.main}>
         <h1>Course Chart</h1>
+        <CircleChart />
       </main>
   )
 }
