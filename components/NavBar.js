8fda0d498d11241e8fece86a95018ce8a19847f7
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-10 20:07:12 -0600

    Add navbar component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
+
 export default function NavBar() {
-    return (
-        <div></div>
-    )
+  return (
+    <nav className={styles.navbar}>
+      <secton className={styles.container}>
+        <div className={styles.logo}>
+          <Image src='/icon.png' height={50} width={50} />
+        </div>
+        <h1>CourseChart</h1>
+      </secton>
+
+      <secton>
+
+      </secton>
+    </nav>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 
+import NavBar from '../components/NavBar'
+
 export default function Home() {
   return (
       <main className={styles.main}>
