4157d6ccd47d909a2e33290ae121cb8658ebfe76
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-10 20:39:38 -0600

    Add additional navigation styling for links

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,13 +12,17 @@ export default function NavBar() {
         <h1>CourseChart</h1>
       </secton>
 
-      <secton className={styles.buttons}>
-        <Link href='/courseDashboard'>
+      <secton className={styles.courseButtons}>
+        <Link href='/'>
           <a>All Courses</a>
         </Link>
 
         <Link href='/courseDashboard'>
           <a>Course 1</a>
+        </Link>
+
+        <Link href='/addCourseForm'>
+          <a className={styles.new}>+ Add New Course</a>
         </Link>
 
         <Link href='/moduleDashboard'>
@@ -29,7 +33,20 @@ export default function NavBar() {
           <a>Module 1</a>
         </Link>
 
+        <Link href='/addModuleForm'>
+          <a className={styles.new}>+ Add New Module</a>
+        </Link>
       </secton>
+
+      <section className={styles.navButtons}>
+        <Link href='/instructions'>
+          <a>Instructions</a>
+        </Link>
+
+        <Link href='/instructions'>
+          <a>About Site</a>
+        </Link>
+      </section>
     </nav>
   )
 }
