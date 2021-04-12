0897867c9b09b0ba952cf119c465f3551dc18735
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-12 10:41:30 -0600

    Start new course form component

--- pages/addCourseForm.js
+++ pages/addCourseForm.js
@@ -1,5 +1,19 @@
+import styles from '../styles/CourseForm.module.scss'
+
 export default function addCourseForm() {
   return (
-    <div></div>
+    <form className={styles.form}>
+      <label htmlFor='institution'>Institution Name: </label>
+      <input type='text' id='institution' />
+
+      <label htmlFor='course'>Course Name: </label>
+      <input type='text' id='course' />
+
+      <label htmlFor='hours'>Credit Hours: </label>
+      <input type='number' id='hours' />
+
+      <label htmlFor='length'>Course Length in Weeks: </label>
+      <input type='number' id='length' />
+    </form>
   )
 }
