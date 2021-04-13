e1f51968774ce3e10a1d7d985b4710b3c4aff00b
Lucas Merchant <lbmerchant93@gmail.com>
2021-04-12 19:35:30 -0600

    Add initial styling to contactDevelopers page

--- pages/contactDevelopers.js
+++ pages/contactDevelopers.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/contactDevelopers.module.scss'
 import Image from 'next/image'
 
 const ContactDevelopers = () => {
@@ -7,72 +7,90 @@ const ContactDevelopers = () => {
         <>
             <main className={styles.main}>
                 <h2>Meet the Developers</h2>
-                <article>
-                    <section>
-                        <h3>Gus Cunningham</h3>
+                <article className={styles.developers}>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Gus Cunningham</h3>
                         <Image 
                             src="/images/Gus-Profile.jpeg" 
                             alt='Gus Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/cunninghamge'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/grayson-cunningham/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/cunninghamge' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/grayson-cunningham/'>LinkedIn Profile</a>
+                        </div>
                     </section>
-                    <section>
-                        <h3>Alice Ruppert</h3>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Alice Ruppert</h3>
                         <Image 
                             src="/images/Alice-Profile.jpeg"
                             alt='Alice Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/srslie'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/aliceruppert/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/srslie' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/aliceruppert/'>LinkedIn Profile</a>
+                        </div>
                     </section>
-                    <section>
-                        <h3>Ely Hess</h3>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Ely Hess</h3>
                         <Image 
                             src="/images/Ely-Profile.jpeg" 
                             alt='Ely Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/elyhess'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/ely-hess/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/elyhess' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/ely-hess/'>LinkedIn Profile</a>
+                        </div>
                     </section>
-                    <section>
-                        <h3>Alia Peterson</h3>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Alia Peterson</h3>
                         <Image 
                             src="/images/Alia-Profile.jpeg" 
                             alt='Alia Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/alia-peterson'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/alia-peterson/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/alia-peterson' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/alia-peterson/'>LinkedIn Profile</a>
+                        </div>                     
                     </section>
-                    <section>
-                        <h3>Cameron Aragon</h3>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Cameron Aragon</h3>
                         <Image 
                             src="/images/Cameron-Profile.jpeg" 
                             alt='Cameron Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/camaragon'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/camaragon/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/camaragon' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/camaragon/'>LinkedIn Profile</a>
+                        </div>                      
                     </section>
-                    <section>
-                        <h3>Lucas Merchant</h3>
+                    <section className={styles.dev}>
+                        <h3 className={styles.h3}>Lucas Merchant</h3>
                         <Image 
                             src="/images/Lucas-Profile.jpeg" 
                             alt='Lucas Github'
-                            width={300}
-                            height={300}
+                            width={100}
+                            height={100}
+                            className={styles.img}
                         /> 
-                        <a href='https://github.com/lbmerchant93'>GitHub Profile</a>
-                        <a href='https://www.linkedin.com/in/lucas-merchant93/'>LinkedIn Profile</a>
+                        <div className={styles.wrapper}>
+                            <a href='https://github.com/lbmerchant93' className={styles.link}>GitHub Profile</a>
+                            <a href='https://www.linkedin.com/in/lucas-merchant93/'>LinkedIn Profile</a>
+                        </div>
                     </section>
                 </article>
             </main>
