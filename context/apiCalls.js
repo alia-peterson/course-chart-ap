06a4d69897e44ebe2d81d3b4793e263f1d1cebea
Cameron Aragon <caragon4695@gmail.com>
2021-04-13 19:13:14 -0600

    Restructure fetch requests with a throw error

--- context/apiCalls.js
+++ context/apiCalls.js
@@ -2,16 +2,26 @@
 // For single course: endpoint = 'courses/:id'
 // For single module: endpoint = 'modules/:id'
 export const getData = (endpoint) => {
-    return fetch(`https://course-chart-be.herokuapp.com/${endpoint}`)
-        .then(response => response.json())
+    const settings = {mode: 'no-cors'}
+    return fetch(`https://course-chart-be.herokuapp.com/${endpoint}`, settings)
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText)
+            }
+            console.log(response)
+            return response
+        })
         .catch(error => console.error(error))
 }
 
 export const postData = (postUrl, postBody) => {
-    const settings = {method: 'POST', body: postBody}
+    const settings = {method: 'POST', body: JSON.stringify(postBody)}
     return fetch(postUrl, settings)
         .then(response => {
-            return (response.status() >= 200 && response.status() < 300)
+            if (!response.ok) {
+                throw Error(response.statusText)
+            }
+            return response
         })
         .catch(error => console.error(error))
 }
