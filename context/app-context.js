ef550b72902c823f0b356113088a662ae119c11a
Cameron Aragon <caragon4695@gmail.com>
2021-04-13 19:13:44 -0600

    Setup context with the fetch responses

--- context/app-context.js
+++ context/app-context.js
@@ -1,11 +1,32 @@
 import { createContext, useContext, useState } from 'react';
 import mockData from './mock-data';
+import { getData, postData } from './apiCalls';
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
-
-    const [sharedState, setSharedState] = useState(mockData)
+    const allCourses = Promise.resolve(getData('courses'))
+        .then(data => {
+            const courseInfo = data.data.map(course => getData(`courses/${course.id}`))
+            Promise.all(courseInfo)
+                .then(data => data)
+        })
+    
+    const post = (postType, postBody) => {
+        let url = 'https://course-chart-be.herokuapp.com/modules'
+        if (postType === 'course') {
+            url = 'https://course-chart-be.herokuapp.com/courses'
+        }
+        Promise.resolve(postData(url, postBody)).then(response => {
+            if (!response) {
+                return alert(`Sorry, there was an error adding your ${postType}.` )
+            }
+            setSharedState(...sharedState, postBody)
+            // based on the value of postType we add it to the necessary array (maybe spread operator)
+        })
+    }
+    const [sharedState, setSharedState] = useState(allCourses)
+    // Cleaning function to assign the null values an empty string ('') for useState()
     const value = { setSharedState, sharedState }
 
     return (
