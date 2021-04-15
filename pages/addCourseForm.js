3df845f32fb05c2912b5522c7877bef8eae56aeb
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2021-04-14 21:38:07 -0600

    Update form styling

--- pages/addCourseForm.js
+++ pages/addCourseForm.js
@@ -26,45 +26,52 @@ export default function addCourseForm() {
 
   return (
     <form className={styles.form} onSubmit={submitForm}>
-      <label htmlFor='institution'>Institution Name: </label>
-      <input
-        type='text'
-        id='institution'
-        className={styles.input}
-        value={institution}
-        onChange={(event) => setInstitution(event.target.value)}
-        required
-        />
 
-      <label htmlFor='course'>Course Name: </label>
-      <input
-        type='text'
-        id='course'
-        className={styles.input}
-        value={course}
-        onChange={(event) => setCourse(event.target.value)}
-        required
-        />
+      <div className={styles.container}>
+        <div className={styles.inner}>
+          <label htmlFor='institution'>Institution Name: </label>
+          <input
+            type='text'
+            id='institution'
+            className={styles.input}
+            value={institution}
+            onChange={(event) => setInstitution(event.target.value)}
+            required
+            />
 
-      <label htmlFor='hours'>Credit Hours: </label>
-      <input
-        type='number'
-        id='hours'
-        className={styles.input}
-        value={hours}
-        onChange={(event) => setHours(event.target.value)}
-        required
-        />
+          <label htmlFor='course'>Course Name: </label>
+          <input
+            type='text'
+            id='course'
+            className={styles.input}
+            value={course}
+            onChange={(event) => setCourse(event.target.value)}
+            required
+            />
+        </div>
 
-      <label htmlFor='length'>Course Length in Weeks: </label>
-      <input
-        type='number'
-        id='length'
-        className={styles.input}
-        value={length}
-        onChange={(event) => setLength(event.target.value)}
-        required
-        />
+        <div className={styles.inner}>
+          <label htmlFor='hours'>Credit Hours: </label>
+          <input
+            type='number'
+            id='hours'
+            className={styles.input}
+            value={hours}
+            onChange={(event) => setHours(event.target.value)}
+            required
+            />
+
+          <label htmlFor='length'>Course Length in Weeks: </label>
+          <input
+            type='number'
+            id='length'
+            className={styles.input}
+            value={length}
+            onChange={(event) => setLength(event.target.value)}
+            required
+            />
+        </div>
+      </div>
 
       <button type='submit' className={styles.button}>Submit</button>
     </form>
