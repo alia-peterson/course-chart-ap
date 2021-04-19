fe85e726335d241badef666e8341cb360de2f037
srslie <aliceruppert@gmail.com>
2021-04-19 13:21:28 -0600

    Add bar functionality

--- context/app-context.js
+++ context/app-context.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import mockData from './mock-data';
-import { getData, postData } from './apiCalls';
+import { getData } from './apiCalls';
 import data from './mock-data';
 
 const AppContext = createContext();
@@ -24,23 +24,7 @@ export function AppWrapper({ children }) {
             activities: activities.data
         })
     }, []);
-
-
-        // console.log(allCourseModules)
-    // const post = (postType, postBody) => {
-    //     let url = 'https://course-chart-be.herokuapp.com/modules'
-    //     if (postType === 'course') {
-    //         url = 'https://course-chart-be.herokuapp.com/courses'
-    //     }
-    //     Promise.resolve(postData(url, postBody)).then(response => {
-    //         if (!response) {
-    //             return alert(`Sorry, there was an error adding your ${postType}.` )
-    //         }
-    //         setSharedState(...sharedState, postBody)
-    //         // based on the value of postType we add it to the necessary array (maybe spread operator)
-    //     })
-    // }
-    // Cleaning function to assign the null values an empty string ('') for useState()
+ 
     const value = {
         setSharedState,
         sharedState
