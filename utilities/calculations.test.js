4400c061788fa84da320ac0834057252701d856a
Cameron Aragon <caragon4695@gmail.com>
2021-04-19 02:55:31 -0600

    Finish jest testing for calculations

--- utilities/calculations.test.js
+++ utilities/calculations.test.js
@@ -3,24 +3,44 @@ import { calculations } from "./calculations";
 
 describe("calculations", () => {
   const allActivities = data.allActivities;
-  console.log(calculations.getPercentages(allActivities, 'activity'));
-
+  
   //   it('should have a get percentages method', () => {
   //     expect(typeof (calculations.getPercentages(allActivities, 'activity'))).toBe('function')
-  //   });  
+  //   });
 
   it("should return all the activities for the course", () => {
-    expect(calculations.getPercentages(allActivities)).toStrictEqual(data.percentageData);
+    expect(calculations.getPercentages(allActivities)).toStrictEqual(
+      data.coursePercentageData
+    );
   });
 
-  it("should return the activity data with a name, perceentage and module id", () => {
-    expect(calculations.getPercentages(allActivities)[0]).toStrictEqual(data.percentageData[0]);
-    expect(calculations.getPercentages(allActivities)[0].name).toStrictEqual("Reading (understand)");
-    expect(calculations.getPercentages(allActivities)[0].percentage).toStrictEqual(7);
-    expect(calculations.getPercentages(allActivities)[0].moduleId).toStrictEqual(1);
+  it("should return the activity data with a name, percentage and module id", () => {
+    expect(calculations.getPercentages(allActivities)[0]).toStrictEqual(
+      data.coursePercentageData[0]
+    );
+    expect(calculations.getPercentages(allActivities)[0].name).toStrictEqual(
+      "Reading (understand)"
+    );
+    expect(
+      calculations.getPercentages(allActivities)[0].percentage
+    ).toStrictEqual(7);
+    expect(
+      calculations.getPercentages(allActivities)[0].moduleId
+    ).toStrictEqual(1);
   });
 
-  it('should return the total percents of all the activities in the course', () => {
-    expect(calculations.getPercentages(allActivities, 'activity')).toStrictEqual(data.percentageData);
+  it("should return the total percents of all the activities in the course", () => {
+    expect(
+      calculations.getPercentages(allActivities, "activity")
+    ).toStrictEqual(data.activtyTotals);
+  });
+
+  it("should return the activity data with the name as the key and the total percentage as a value", () => {
+    expect(
+      calculations.getPercentages(allActivities, "activity")
+    ).toHaveProperty("Reading (understand)");
+    expect(
+      calculations.getPercentages(allActivities, "activity")
+    ).toHaveProperty("Reading (understand)", 32);
   });
 });
