4e75ff6a439c03731ecb00150a695a2a3b77a7d5
srslie <aliceruppert@gmail.com>
2021-04-18 23:20:53 -0600

    Migrate styling to scss

--- pages/addModuleForm.js
+++ pages/addModuleForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useAppContext } from '../context/app-context'
-import {activities} from '../context/activities'
+import { activities } from '../context/activities'
+import styles from '../styles/addModuleForm.module.scss'
 
 export default function addModuleForm() {
   const { sharedState, setSharedState } = useAppContext()
@@ -8,22 +9,18 @@ export default function addModuleForm() {
   const moduleName = useRef('')
   const description = useRef('')
 
-  const refs = 
+  const states = 
     Object.fromEntries(Object.keys(activities).map(key => {
       return [key, useState(0)]
     }))
 
-  //we need to figure out how to default/grab this chosen course to enable us to find the right hours per week to show the user as they're seeing the inputs and totals
-  // const chosenCourse = sharedState.courses.find(c => c.name === course)
-  // const [timeMeter, setTimeMeter] = useState(chosenCourse.creditHours)
-
-  // const courses = sharedState.courses.map((course, i) => {
-  //   return (
-  //     <option value={course} >
-  //       {course.name}
-  //     </option>
-  //   )
-  // })
+  const courses = sharedState.courses.map((course) => {
+    return (
+      <option value={course} >
+        {course.name}
+      </option>
+    )
+  })
 
   const addModule = event => {
     event.preventDefault()
@@ -32,7 +29,7 @@ export default function addModuleForm() {
       id: Date.now(),
       course: chosesCourse,
       number: chosenCourse.modules.length,
-      ...refs
+      ...states
     }
     console.log('MODULEPOST', modulePost)
 
@@ -42,79 +39,33 @@ export default function addModuleForm() {
   const makeInputs = (activities) => {
     
     const allInputs = Object.keys(activities).map(key => (
-        <div className="inputStyle">
-          <div className="titleMinutes">
-            <p className="minutesTotal">{refs[key][0] * activities[key].multiplier}</p>
-            <p className="title">{activities[key].name}</p>
+        <div className={styles.inputStyle}>
+
+          <div className={styles.titleMinutes}>
+            <p className={styles.minutesTotal}>
+              {states[key][0] * activities[key].multiplier}
+            </p>
+            <p className={styles.title}>
+              {activities[key].name}
+            </p>
           </div>
-          <label className="circleLabel" htmlFor={key} aria-label={activities[key].name}>{activities[key].metric}</label>
-          <input className="circleInput" value={refs[key][0]} id={activities[key].id} type="number" onChange={(event) => refs[key][1](event.target.value)}/>
-          <div className="description">
+
+          <label 
+            className={styles.circleLabel} 
+            htmlFor={key} 
+            aria-label={activities[key].name}>
+              {activities[key].metric}
+          </label>
+          <input 
+            className={styles.circleInput} 
+            value={states[key][0]} 
+            id={activities[key].id} 
+            type="number" 
+            onChange={(event) => states[key][1](event.target.value)}/>
+          <div className={styles.description}>
             <p>{activities[key].description}</p>
           </div>
-          <style jsx>{`
-          .inputStyle {
-            display: flex;
-            flex-direction: row;
-            margin-bottom: 2rem;
-            z-index: 1;
-            align-items: baseline;
-            
-          }
-          .titleMinutes {
-            display: flex;
-            flex-direction: row;
-            width: 40%;
-            height: 4rem;
-            text-align: center;
-          }
-          .title {
-            height: 100%;
-            width: 20rem;
-            border: 2px solid #1A3D59;
-            padding: 2.5rem 2rem 0rem 2rem;
-            text-align: center;
-            margin: 0.7rem 4rem 3rem -2rem;
-            font-weight: bold;
-            padding: 1rem;
-            margin: 2.5rem 4rem 5rem -2rem;
-            word-break: break-word;
-          }
-          .minutesTotal {
-            height: 100%;
-            width: 5rem;
-            background-color: #E6E6E6;
-            padding: 1rem;
-            text-align: center;
-            margin: 2.5rem 2rem 5rem 2rem
-          }
-          .circleLabel {
-            width: 15%;
-            padding-left: 2rem;
-            background-color: #E6E6E6;
-            height: 2rem;
-          }
-          .circleInput {
-            padding: 1.5rem;
-            width: 8rem;
-            height: 8rem;
-            border: 7px solid #1A3D59;
-            border-radius: 70px;
-            font-size: large;
-            z-index: 2;
-          }
-          .description {
-            background-color: #E6E6E6;
-            width: 40%;
-            height: 4rem;
-            text-align: center;
-            position: relative;
-            padding-left: 3rem;
-            z-index: 1;
-            left: -2rem;
-            bottom: .5rem;
-          }
-        `}</style>
+
         </div>
       )
     )
@@ -122,90 +73,78 @@ export default function addModuleForm() {
   }
   
   return (
-    <>
+    <div className={styles.addModuleForm}>
       <h1>Add A Module</h1>
 
       <form onSubmit={addModule}>
-        <div className="moduleMetaData">
-          <label htmlFor="course" aria-label="Course">Course:</label>
-          <select ref={course} id="course">
+        <div className={styles.moduleMetaData}>
+          <label 
+            className={styles.formLabel} 
+            htmlFor="course" 
+            aria-label="Course">
+              Course:
+          </label>
+          <select 
+            className={styles.formSelect} 
+            ref={course} 
+            id="course">
+
             <option value="">--Please choose an option--</option>
-            {/*{courses}*/}
+            {courses}
+
           </select>
 
-          <label htmlFor="module-name" aria-label="Module Name">Module  Name</label>
-          <input ref={moduleName} id="module-name" type="text" required/>
+          <label 
+            className={styles.formLabel} 
+            htmlFor="module-name" 
+            aria-label="Module Name">
+              Module Name
+          </label>
+          <input 
+            className={styles.formInput} 
+            ref={moduleName} 
+            id="module-name" 
+            type="text" 
+            required />
 
-          <label htmlFor="description"  aria-label="description">Description</label>
-          <input ref={description} id="description" type="text"/>
+          <label 
+            className={styles.formLabel} 
+            htmlFor="description"  
+            aria-label="description">
+              Description
+          </label>
+          <input 
+            className={styles.formInput} 
+            ref={description} 
+            id="description" 
+            type="text"/>
 
-          <style jsx>{`
-            .moduleMetaData {
-              display: flex;
-              flex-flow: column;
-              width: 50%;
-              margin-left: 25%;
-            }
-            input, select, label {
-              padding: .5rem;
-            }
-            label {
-              padding-top: 1rem;
-              padding-right: 2rem;
-            }
-          `}</style>
         </div>
-        
-      <div className="timeBar">
-        <p className="timeLabel">Total Recommended Time per Week: {10}</p>
-        <div className="timeMeter">
-          <div className="timeUsed" ></div>
-          
-      </div>
-      <style jsx>{`
-          .timeBar {
-            text-align: center;
-            margin-top: 3rem;
-          }
-            .timeMeter {
-              height: 1rem; 
-              background-color: #E6E6E6;
-              border: 2px solid #1A3D59;
-              margin-bottom: 2rem;
-              z-index: 1;
-              margin-top: 1rem;
-            }
-            .timeUsed {
-              width: 50%;
-              height: 100%;
-              z-index: 2;
-              background-color: #FAC70F;
-            }
-          `}</style>
-      </div>
 
-      <div className="topLabels" >
-        <p className="topLabelMinutes">Total Minutes</p>
-        <p className="topLabelInput">INPUT 🖊</p>
+      <div className={styles.topLabels}>
+        <p className={styles.topLabelMinutes}>
+          Total Minutes
+        </p>
+        <p className={styles.topLabelInput}>
+          INPUT 🖊
+        </p>
         <p>Time Per Task</p>
-        <style jsx>{`
-          .topLabels {
-            display: flex;
-            flex-direction: row;
-            margin-left: 5%;
-          }
-          .topLabelMinutes {
-            margin-right: 28rem;
-          }
-          .topLabelInput {
-            margin-right: 8rem;
-          }
-        `}</style>
       </div>
 
         {makeInputs(activities)}
         
       </form>
-      </>
+  
+      <div className={styles.timeBar}>
+        <p className={styles.timeLabel}>
+          Total Recommended Time per Week: {10}
+        </p>
+        <div className={styles.timeMeter}>
+          <div className={styles.timeUsed} >
+          </div>
+        </div>
+      </div>
+
+      </div>
   )
 }
