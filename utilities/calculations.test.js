ae5dffe03d49d7a37b7233a19cbdbb6471eea7ba
Cameron Aragon <caragon4695@gmail.com>
2021-04-19 02:41:13 -0600

    Add jest tests for the percentages by mod function

--- utilities/calculations.test.js
+++ utilities/calculations.test.js
@@ -1,18 +1,26 @@
-import data from '../context/mock-data'
-import {calculations} from './calculations'
+import data from "../context/mock-data";
+import { calculations } from "./calculations";
 
-describe('calculations', () => {
-    const allActivities = data.allActivities
-  console.log(calculations)
-  
-//   it('should have a get percentages method', () => {
-//     expect(typeof (calculations.getPercentages(allActivities, 'activity'))).toBe('function')
-//   });
+describe("calculations", () => {
+  const allActivities = data.allActivities;
+  console.log(calculations.getPercentages(allActivities, 'activity'));
 
-  it('should return all the activities for the course', () => {
-     expect(calculations.getPercentages(allActivities, 'activity'))
-  })
+  //   it('should have a get percentages method', () => {
+  //     expect(typeof (calculations.getPercentages(allActivities, 'activity'))).toBe('function')
+  //   });  
 
-  it 
+  it("should return all the activities for the course", () => {
+    expect(calculations.getPercentages(allActivities)).toStrictEqual(data.percentageData);
+  });
+
+  it("should return the activity data with a name, perceentage and module id", () => {
+    expect(calculations.getPercentages(allActivities)[0]).toStrictEqual(data.percentageData[0]);
+    expect(calculations.getPercentages(allActivities)[0].name).toStrictEqual("Reading (understand)");
+    expect(calculations.getPercentages(allActivities)[0].percentage).toStrictEqual(7);
+    expect(calculations.getPercentages(allActivities)[0].moduleId).toStrictEqual(1);
+  });
+
+  it('should return the total percents of all the activities in the course', () => {
+    expect(calculations.getPercentages(allActivities, 'activity')).toStrictEqual(data.percentageData);
+  });
 });
-
