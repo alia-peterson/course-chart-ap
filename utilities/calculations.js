306acfab3126e345ff8810fda336237f4a0c0a7a
Cameron Aragon <caragon4695@gmail.com>
2021-04-20 00:23:57 -0600

    Update calculations methods for charts Co-authored-by: Alice Ruppert <68885499+srslie@users.noreply.github.com>

--- utilities/calculations.js
+++ utilities/calculations.js
@@ -1,52 +1,43 @@
 export const calculations = {
-  getPercentages(activities, type = '') {
-    
-    const activityTotals = activities.map(activity => {
-      return activity.minutes
-    })
+  filterModuleActivities(activities, id) {
+    return activities.filter((activity) => activity.moduleId === id);
+  },
 
-    const totalMinsAllActivities = activityTotals.reduce((total, activity) => {
-      return total + activity
-    }, 0)
+  getModulePercentages(moduleActivities) {
+    const moduleActivityTotal = moduleActivities.reduce((total, activity) => {
+      return total + activity.minutes;
+    }, 0);
 
-    const compiledActivityData = activityTotals.map((total, i) => {
+    return moduleActivities.map((activity) => {
+      const decimal = activity.minutes / moduleActivityTotal;
+      const percentage = decimal * 100;
+      const truncated = Math.trunc(percentage);
       return {
-        name: activities[i].activityName,
-        percentage: Math.round(total / totalMinsAllActivities * 100),
-        moduleId: activities[i].moduleId
-      }
-    })
+        [activity.activityName]: truncated,
+      };
+    });
+  },
 
-    if (type === 'activity') {
-      return compiledActivityData.reduce((acc, curr) => {
-        if (acc[curr.name]) {
-          acc[curr.name] += curr.percentage
+  getPercentages(activities, activityTypes) {
+    const typeIdAndArrays = activityTypes.map((type) => [type.id, 0]);
+    const types = Object.fromEntries(typeIdAndArrays);
+    activities.forEach((activity) => {
+      types[activity.activityId] += activity.minutes;
+    });
 
-        } else {
-          acc[curr.name] = curr.percentage
-        }
+    const typeTotalsArray = Object.values(types);
 
-        return acc
-      }, {})
+    const totalCourseActivities = typeTotalsArray.reduce((total, activity) => {
+      return total + activity;
+    }, 0);
 
-    } else {
-      return compiledActivityData
-    }
-  }
-}
-
-const numOfMods = (course) => course.modules.map(mod => mod.name)
-
-const specificActivities = (activityTotals, label) => activityTotals.filter(activity => {
-    return activity.activityName.includes(label)
-})
-
-export const formatDataForBarChart = (course, label, activityTotals) => numOfMods(course).reduce((accumulator, modName) => {
-    let found = specificActivities(activityTotals, label).find(mod => mod.moduleName === modName)
-    if (found) {
-        accumulator[modName] = found.minutes 
-    } else {
-        accumulator[modName] = 0
-    }
-    return accumulator
-}, {})
+    const percentages = typeTotalsArray.map((total, i) => {
+      const decimal = total / totalCourseActivities;
+      const percent = decimal * 100;
+      return {
+        [activityTypes[i].name]: Math.trunc(percent),
+      };
+    });
+    return percentages;
+  },
+};
