5f58ee7dfc2a629d3c2ee2215782b0e3ebdf3bdb
Cameron Aragon <caragon4695@gmail.com>
2021-04-20 14:49:02 -0600

    Rename mock data to test data

---
+++ context/test-data.js
@@ -0,0 +1,503 @@
+const data = {
+  course: {
+    "id": 1,
+    "name": "Foundations of Nursing",
+    "institution": "Colorado Nursing College",
+    "creditHours": 3,
+    "length": 8,
+    "modules": [
+        {
+            "id": 1,
+            "name": "Module 1",
+            "number": 1,
+            "courseId": 1,
+            "moduleActivities": []
+        },
+        {
+            "id": 2,
+            "name": "Module 2",
+            "number": 2,
+            "courseId": 1,
+            "moduleActivities": []
+        },
+        {
+            "id": 3,
+            "name": "Module 3",
+            "number": 3,
+            "courseId": 1,
+            "moduleActivities": []
+        },
+        {
+            "id": 4,
+            "name": "Module 4",
+            "number": 4,
+            "courseId": 1,
+            "moduleActivities": []
+        },
+        {
+            "id": 5,
+            "name": "Module 5",
+            "number": 5,
+            "courseId": 1,
+            "moduleActivities": []
+        }
+      ]
+    },
+  
+  label: 'Reading (understand)',
+
+  allActivityTotals: [
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 642
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 318
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 396
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 630
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 312
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 72
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 60
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 48
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 84
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 60
+    },
+    {
+        "activityId": 4,
+        "activityName": "Writing (reflection)",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 90
+    },
+    {
+        "activityId": 4,
+        "activityName": "Writing (reflection)",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 180
+    },
+    {
+        "activityId": 4,
+        "activityName": "Writing (reflection)",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 90
+    },
+    {
+        "activityId": 5,
+        "activityName": "Learning Objects (matching/multiple choice)",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 70
+    },
+    {
+        "activityId": 5,
+        "activityName": "Learning Objects (matching/multiple choice)",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 50
+    },
+    {
+        "activityId": 5,
+        "activityName": "Learning Objects (matching/multiple choice)",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 40
+    },
+    {
+        "activityId": 5,
+        "activityName": "Learning Objects (matching/multiple choice)",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 30
+    },
+    {
+        "activityId": 5,
+        "activityName": "Learning Objects (matching/multiple choice)",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 50
+    },
+    {
+        "activityId": 6,
+        "activityName": "Learning Objects (case study)",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 40
+    },
+    {
+        "activityId": 6,
+        "activityName": "Learning Objects (case study)",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 20
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 95
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 71
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 86
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 75
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 62
+    },
+    {
+        "activityId": 10,
+        "activityName": "Discussion Boards",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 60
+    },
+    {
+        "activityId": 10,
+        "activityName": "Discussion Boards",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 60
+    },
+    {
+        "activityId": 10,
+        "activityName": "Discussion Boards",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 60
+    },
+    {
+        "activityId": 10,
+        "activityName": "Discussion Boards",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 60
+    },
+    {
+        "activityId": 11,
+        "activityName": "Quizzes",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 675
+    },
+    {
+        "activityId": 11,
+        "activityName": "Quizzes",
+        "moduleId": 2,
+        "moduleName": "Module 2",
+        "minutes": 150
+    },
+    {
+        "activityId": 11,
+        "activityName": "Quizzes",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 360
+    },
+    {
+        "activityId": 11,
+        "activityName": "Quizzes",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 585
+    },
+    {
+        "activityId": 11,
+        "activityName": "Quizzes",
+        "moduleId": 5,
+        "moduleName": "Module 5",
+        "minutes": 450
+    },
+    {
+        "activityId": 13,
+        "activityName": "Self Assessments",
+        "moduleId": 1,
+        "moduleName": "Module 1",
+        "minutes": 50
+    },
+    {
+        "activityId": 13,
+        "activityName": "Self Assessments",
+        "moduleId": 3,
+        "moduleName": "Module 3",
+        "minutes": 50
+    },
+    {
+        "activityId": 13,
+        "activityName": "Self Assessments",
+        "moduleId": 4,
+        "moduleName": "Module 4",
+        "minutes": 50
+    },
+],
+
+  courseActivityTotals: [
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 69,
+        "moduleName": "Wizardry",
+        "minutes": 12
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 18
+    },
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 104,
+        "moduleName": "Horses",
+        "minutes": 18
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 48
+    },
+    {
+        "activityId": 3,
+        "activityName": "Writing (research)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 360
+    },
+    {
+        "activityId": 8,
+        "activityName": "Videos",
+        "moduleId": 81,
+        "moduleName": "oh",
+        "minutes": 14
+    },
+    {
+        "activityId": 14,
+        "activityName": "Miscellaneous",
+        "moduleId": 81,
+        "moduleName": "oh",
+        "minutes": 5
+    }
+  ],
+
+  moduleActivities: [
+    {
+        "activityId": 1,
+        "activityName": "Reading (understand)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 18
+    },
+    {
+        "activityId": 2,
+        "activityName": "Reading (study guide)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 48
+    },
+    {
+        "activityId": 3,
+        "activityName": "Writing (research)",
+        "moduleId": 74,
+        "moduleName": "Wowie",
+        "minutes": 360
+    }
+],
+
+  activityTypes: [
+    {
+        "id": 1,
+        "name": "Reading (understand)",
+        "description": "130 wpm; 10 pages per hour",
+        "metric": "# of pages",
+        "multiplier": 6,
+        "color": "#FF6384"
+    },
+    {
+        "id": 2,
+        "name": "Reading (study guide)",
+        "description": "65 wpm; 5 pages per hour",
+        "metric": "# of pages",
+        "multiplier": 12,
+        "color": "#36A2EB"
+    },
+    {
+        "id": 3,
+        "name": "Writing (research)",
+        "description": "6 hours per page (500 words, single-spaced)",
+        "metric": "# of pages",
+        "multiplier": 360,
+        "color": "#FFCE56"
+    },
+    {
+        "id": 4,
+        "name": "Writing (reflection)",
+        "description": "90 minutes per page (500 words, single-spaced)",
+        "metric": "# of pages",
+        "multiplier": 90,
+        "color": "#1dea49"
+    },
+    {
+        "id": 5,
+        "name": "Learning Objects (matching/multiple choice)",
+        "description": "10 minutes per object",
+        "metric": "# of LOs",
+        "multiplier": 10,
+        "color": "#ef1aae"
+    },
+    {
+        "id": 6,
+        "name": "Learning Objects (case study)",
+        "description": "20 minutes per object",
+        "metric": "# of LOs",
+        "multiplier": 20,
+        "color": "#0b04fa"
+    },
+    {
+        "id": 7,
+        "name": "Lecture",
+        "description": "Factor 1.25x the actual lecture runtime",
+        "metric": "# of minutes",
+        "multiplier": 1.25,
+        "color": "#9a1aa0"
+    },
+    {
+        "id": 8,
+        "name": "Videos",
+        "description": "Factor the full length of video",
+        "metric": "# of minutes",
+        "multiplier": 1,
+        "color": "#3f218c"
+    },
+    {
+        "id": 9,
+        "name": "Websites",
+        "description": "10-20 minutes",
+        "metric": "",
+        "multiplier": 1,
+        "color": "#ebfc05"
+    },
+    {
+        "id": 10,
+        "name": "Discussion Boards",
+        "description": "250 words/60 minutes for initial post or 2 replies",
+        "metric": "# of discussion boards",
+        "multiplier": 60,
+        "color": "#42e6d0"
+    },
+    {
+        "id": 11,
+        "name": "Quizzes",
+        "description": "Average 1.5 minutes per question",
+        "metric": "# of questions",
+        "multiplier": 1.5,
+        "color": "#bb0935"
+    },
+    {
+        "id": 12,
+        "name": "Exams",
+        "description": "Average 1.5 minutes per question",
+        "metric": "# of questions",
+        "multiplier": 1.5,
+        "color": "#5d9b35"
+    },
+    {
+        "id": 13,
+        "name": "Self Assessments",
+        "description": "Average 1 minute per question",
+        "metric": "# of questions",
+        "multiplier": 1,
+        "color": "#ffa500"
+    },
+    {
+        "id": 14,
+        "name": "Miscellaneous",
+        "description": "any additional assignments not listed",
+        "metric": "",
+        "multiplier": 1,
+        "color": "#ff2500"
+    }
+]
+};
+
+export default data;
