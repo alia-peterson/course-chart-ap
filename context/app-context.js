5b647d32fd7e2c397886538ddcf3698cf6001845
Cameron Aragon <caragon4695@gmail.com>
2021-04-20 00:29:52 -0600

    Update app context functionality Co-authored-by: Alice Ruppert <68885499+srslie@users.noreply.github.com>

--- context/app-context.js
+++ context/app-context.js
@@ -1,46 +1,36 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import mockData from './mock-data';
-import { getData, postData } from './apiCalls';
+import { getData } from './apiCalls';
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
+    const [hasBeenDeleted, setHasBeenDeleted] = useState(false)
     const [sharedState, setSharedState] = useState({
         courses: [{
             modules:[]
         }],
         currentCourse: '',
-        currentModule: ''
+        currentModule: '',
+        currentCourseActivityTotals: [],
+        activities: []
     })
 
     useEffect( async () => {
-        getData('courses')
-            .then(courses => {
-                setSharedState({
-                    courses: courses.data
-                })
-            })
+        const courses = await getData('courses')
+        const activities = await getData('activities')
+        setSharedState({
+            ...sharedState,
+            courses: courses.data,
+            activities: activities.data
+        })
     }, []);
 
-
-        // console.log(allCourseModules)
-    // const post = (postType, postBody) => {
-    //     let url = 'https://course-chart-be.herokuapp.com/modules'
-    //     if (postType === 'course') {
-    //         url = 'https://course-chart-be.herokuapp.com/courses'
-    //     }
-    //     Promise.resolve(postData(url, postBody)).then(response => {
-    //         if (!response) {
-    //             return alert(`Sorry, there was an error adding your ${postType}.` )
-    //         }
-    //         setSharedState(...sharedState, postBody)
-    //         // based on the value of postType we add it to the necessary array (maybe spread operator)
-    //     })
-    // }
-    // Cleaning function to assign the null values an empty string ('') for useState()
     const value = {
         setSharedState,
-        sharedState
+        sharedState,
+        hasBeenDeleted,
+        setHasBeenDeleted
     }
 
     return (
