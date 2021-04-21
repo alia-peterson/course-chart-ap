86667d950ae38d9b59d930ab50d699970f5a8db7
srslie <aliceruppert@gmail.com>
2021-04-20 20:55:50 -0600

    Fix import typo

--- pages/moduleDashboard.js
+++ pages/moduleDashboard.js
@@ -5,7 +5,7 @@ import { calculations } from "../utilities/calculations";
 import { deleteData } from '../context/apiCalls'
 import { useRouter } from 'next/router'
 
-import styles from "../styles/moduleDashBoard.module.scss";
+import styles from "../styles/moduleDashboard.module.scss";
 import { activities } from "../context/activities";
 import courseDashboard from "./courseDashboard";
 
