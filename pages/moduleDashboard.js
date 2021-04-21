f5f3bf86500b9be01b0b6f18776919e6ca6073b5
srslie <aliceruppert@gmail.com>
2021-04-20 19:07:38 -0600

    Clean console logs add styling

--- pages/moduleDashboard.js
+++ pages/moduleDashboard.js
@@ -68,7 +68,7 @@ export default function moduleDashboard() {
           <section className={styles.moduleActivitySideInfo}>
             <div className={styles.moduleActivityNameNotes}>
               <p style={{backgroundColor: `${color}`, color: `white`}}>{name}</p>
-              <p>{notes}</p>
+              <p className={styles.moduleNotes}>{notes}</p>
             </div>
             <div className={styles.moduleActivityTimeNotes}>
               <p>{description}</p>
@@ -103,9 +103,10 @@ export default function moduleDashboard() {
         {percentages.length && 
           <CircleChart data={percentages} />}
           {activityInputs(module.moduleActivities)}
-          <button className={styles.deleteButton} onClick={deleteMod}>Delete Module</button>
-          <button className={styles.deleteButton} onClick={editMod}>Edit Module</button>
-          
+          <div className={styles.buttons}>
+            <button className={styles.editButton} onClick={editMod}>Edit Module</button>
+            <button className={styles.deleteButton} onClick={deleteMod}>Delete Module</button>
+          </div>
       </>
       }
     </>
