d23ff78cfd9c76a30ddf41b05ede7179545ffd89
Cameron Aragon <caragon4695@gmail.com>
2021-04-20 19:58:36 -0600

    Pass the cypress tests for dashboard Co-authored-by: Alice Ruppert <68885499+srslie@users.noreply.github.com>

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -8,23 +8,29 @@ context('Dashboard View', () => {
     cy.visit(baseUrl)
   })
 
+  it('Should display the heading of Course Chart on initial display', () => {
+    cy.get('h1').should('contain', 'Course Chart')
+  })
+
   it('Has a navigation bar with course and module buttons', () => {
     cy.get('nav').find('img').should('be.visible')
       .get('nav').find('h1').should('have.text', 'CourseChart')
       .get('nav').find('a').eq(0).should('have.text', 'Home')
-      .get('nav').find('a').eq(1).should('have.text', '+ Add New Course')
-      .get('nav').find('a').eq(2).should('have.text', 'Nursing 101')
-      .get('nav').find('a').eq(3).should('have.text', 'Foundations of Nursing')
+      .get('nav').find('a').eq(1).should('have.text', 'Nursing 101')
+      .get('nav').find('a').eq(2).should('have.text', 'Foundations of Nursing')
+      .get('nav').find('a').eq(3).should('have.text', '+ Add New Course')
       .get('nav').find('a').eq(4).should('have.text', 'Instructions')
       .get('nav').find('a').eq(5).should('have.text', 'About Site')
   })
 
   it('Should change url path to selected addCourseForm page', () => {
-    cy.get('nav').find('a').eq(1).click()
-      .url().should('include', 'addCourseForm')
+    cy
+    .wait(1000)
+    .get('nav').find('a').eq(3).click()
+    .url().should('include', 'addCourseForm')
   })
 
-  it.only('Should change url path to selected courseDashboard page', () => {
+  it('Should change url path to selected courseDashboard page', () => {
     cy.get('nav').find('a[id=1]').click()
       .url().should('include', 'courseDashboard')
   })
@@ -39,7 +45,7 @@ context('Dashboard View', () => {
   it('Should change url path to selected addModuleForm page', () => {
     cy.intercept('GET', course1Api, { fixture: 'course1-api' })
       .get('nav').find('a[id=1]').click().wait(500)
-      .get('nav').find('a').eq(13).click()
+      .get('nav').find('a').eq(11).click()
       .url().should('include', 'addModuleForm')
   })
 })
